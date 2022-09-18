e5d360b30f5a570901845f07cd120c0fbcdcd5d0
Arthi-6 <113583447+Arthi-6@users.noreply.github.com>
2022-09-18 22:18:26 +0530

    Create add integer to decimal.java

---
+++ Google Assignments/Assignment -1/add integer to decimal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float a = sc.nextInt();
+    float b = sc.nextFloat();
+    System.out.print(a+b);
+  }
+}
