0afa5c57685dde2b0c9116f54ce7d068327c0f2b
Arthi-6 <113583447+Arthi-6@users.noreply.github.com>
2022-09-19 11:27:21 +0530

    Create sorting the sentence.java

---
+++ Microsoft/Homework Assignments/Week -1/sorting the sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String sortSentence(String s) {
+        String str[]=s.split(" ");
+        String res[] = new String[str.length];
+        int temp;
+        for(int i=0;i<str.length;i++){
+            temp=str[i].charAt(str[i].length()-1)-'0';
+            res[temp-1]=str[i].substring(0,str[i].length()-1);
+        }
+        String ns ="";
+        for(int i=0;i<res.length;i++){
+            ns=ns+res[i];
+            if(i!=res.length-1){
+                ns=ns+" ";
+            }
+        }
+        return ns;
+    }
+}
